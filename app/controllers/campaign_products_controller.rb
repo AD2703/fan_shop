53e5654b8df12ba85ee1f7962f8cb268a69aef50
Alejandro Doria <alexdoria.7@gmail.com>
2021-11-23 11:45:18 +0100

    controllers

---
+++ app/controllers/campaign_products_controller.rb
@@ -0,0 +1,2 @@
+class CampaignProductsController < ApplicationController
+end
---
+++ test/controllers/campaign_products_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class CampaignProductsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
