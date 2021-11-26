068a94a23819cdbbdfea5d5b7458e6b1d8ff2128
carles espunya <carlespuna@gmail.com>
2021-11-26 12:48:41 +0100

    order items

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -2,7 +2,7 @@ class OrderItemsController < ApplicationController
   def create
     @order_item = OrderItem.new(order_item_params)
     @order_item.order = Order.find(params[:order_id])
-    @order_item.value = params[:order_item][:quantity] * @order_item.product.price
+    @order_item.value = @order_item.quantity * @order_item.product.price
     @order_item.save
 
     redirect_to campaign_campaign_products_path(@order_item.campaign)
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,12 +1,5 @@
 class OrdersController < ApplicationController
   def create
     @order = Order.create(user: current_user)
-    @total_price = 0
-    if @order.order_items
-      @order.order_items.each do |oi|
-        @total_price += oi.value
-      end
-    end
-    @order.total_price = @total_price
   end
 end
