1c43209c56159d926c5df919b6a51aef53f1b08b
Alejandro Doria <alexdoria.7@gmail.com>
2021-12-01 16:31:40 +0100

    stripe success url

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -35,8 +35,8 @@ class OrdersController < ApplicationController
     session = Stripe::Checkout::Session.create(
       payment_method_types: ['card'],
       line_items: @line_items_array,
-      success_url: order_url(@order),
-      cancel_url: order_url(@order)
+      success_url: "https://www.fanshop.digital/orders/#{@order.id}",
+      cancel_url: "https://www.fanshop.digital/orders/#{@order.id}"
     )
 
     @order.update!(checkout_session_id: session.id)
